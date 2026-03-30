30612c1f9e5ed100af4ba6c7feb63973556bdb35
Kostya <fesenko.kostya576@gmail.com>
2026-03-30 16:52:49 +0300

    create user models

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+
+from db.database import Base, engine
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
 
 
 app = FastAPI(
-    title="ping me Project API", description="FastAPI with database PostgreSQL.", version="0.1.0"
+    title="ping me Project API",
+    description="FastAPI with database PostgreSQL.",
+    version="0.1.0",
+    lifespan=lifespan,
 )
 
 
