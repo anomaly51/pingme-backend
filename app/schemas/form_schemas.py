7042a459585c30d7e59fe3ce60417c371851c173
Kostya <fesenko.kostya576@gmail.com>
2026-04-03 12:08:32 +0300

    user json form

---
+++ app/schemas/form_schemas.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+from pydantic import BaseModel
+
+
+class FormBase(BaseModel):
+    title: str
+    form_structure: dict[str, Any]
+    schedule_crons: list[str]
+
+
+class FormCreate(FormBase):
+    pass
+
+
+class FormUpdate(FormBase):
+    pass
