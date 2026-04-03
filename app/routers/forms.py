5574f9ce9092fe60e30d49e01c9d2ce50b2e6a03
Kostya <fesenko.kostya576@gmail.com>
2026-04-03 12:14:48 +0300

    add json forms

--- app/routers/forms.py
+++ app/routers/forms.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from security import get_current_user
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.security import get_current_user
 from db.database import get_db
 
 from ..models.user_model import Form, User
