684afb03edc2186871fba3daae08c7aeacd31070
Kostya <fesenko.kostya576@gmail.com>
2026-04-04 20:52:32 +0300

    add google table

--- app/routers/study_tracking_router.py
+++ app/routers/study_tracking_router.py
@@ -1,6 +1,3 @@
-import os
-
-import gspread
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -8,15 +5,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.models.user_model import User
 from app.schemas.study_schemas import StudyTrackingCreate
 from app.security import get_current_user
+from app.services.study_tracking import update_study_data
 from db.database import get_db
 
 
 router = APIRouter(prefix="/tracking", tags=["Tracking"])
-
-try:
-    gc = gspread.service_account(filename="google_services.json")
-except Exception as e:
-    print(f"Google credentials load error: {e}")
 
 
 async def get_current_user_obj(
@@ -32,12 +25,5 @@ async def get_current_user_obj(
 
 @router.post("/study")
 async def add_study_tracking(data: StudyTrackingCreate, user: User = Depends(get_current_user_obj)):
-    try:
-        sh = gc.open_by_key(os.getenv("GOOGLE_TABLE_ID"))
-        worksheet = sh.sheet1
-        row_to_append = [user.email, data.activity, data.hours_spent]
-        worksheet.append_row(row_to_append)
-        return {"message": "Data added to Google Sheet"}
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Google Sheets Error: {str(e)}")
+    await update_study_data(email=user.email, activity=data.activity, hours=data.hours_spent)
+    return {"message": "Data successfully saved to Google Sheets!"}
