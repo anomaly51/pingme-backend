0d7a71297e3e21c56d129ee9bc96aa993349073c
Kostya <fesenko.kostya576@gmail.com>
2026-05-22 10:07:49 +0300

    Revert "fix: extend access token lifetime"
    This reverts commit 1d011a23637889a1b659d658e5a240d60c9df2dd.

--- app/core/security.py
+++ app/core/security.py
@@ -9,7 +9,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60 * 24 * 30))
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 7))
 
 
